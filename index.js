cdd34453f2fb4568399e0be3420708a55f75feeb
Ourobore <lchapren@student.42.fr>
2022-06-09 15:20:12 +0200

    Continued to do some tests

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-let counter = 0;
+console.log("[index.js] Loaded");
 
-function incrementCounter() {
-  document.getElementById("counter-label").innerHTML = ++counter;
+const audioFiles = new Array();
+function addFile(file) {
+  console.log("New file: ", file);
+  audioFiles.push(file);
+  console.log("Files: ", audioFiles);
 }
-
-function store(key, value) {
-  localStorage.setItem(key, value);
-}
-
-console.log("Loaded at page startup");
 
 window.onload = function () {
   // Web Audio API tests
-  const audioElement = document.querySelector("audio");
+  const audioElement = document.getElementById("newAudio");
 
-  // Analyzer
-  audioElement.onplay = function () {
-    // Init and audio file loading
+  audioElement.onload = function () {
+    // Analyzer setup
     const audioContext = new AudioContext();
-    // console.log("Audio source: ", audioElement.currentSrc);
     const audioTrack = audioContext.createMediaElementSource(audioElement);
     const audioAnalyzer = audioContext.createAnalyser();
     audioTrack.connect(audioAnalyzer);
     audioAnalyzer.connect(audioContext.destination);
-    audioAnalyzer.fftSize = 16384;
-    const bufferLength = audioAnalyzer.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    console.log("Data array: ", dataArray);
-    // setTimeout(() => console.log("Data array LATER: ", dataArray), 10000);
-    const intervalID = setInterval(() => {
-      for (const value in dataArray) {
-        console.log("HZ: ", value);
-      }
-    }, 1000);
   };
 };
